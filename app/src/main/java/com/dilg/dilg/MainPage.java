8699715cd6ce3960d8da8ab53e0ca9e88038e321
arvince <alcaidearvince@gmail.com>
2017-06-07 13:54:46 +0800

    add functions for button 1

--- app/src/main/java/com/dilg/dilg/MainPage.java
+++ app/src/main/java/com/dilg/dilg/MainPage.java
@@ -1,13 +1,54 @@
 package com.dilg.dilg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainPage extends AppCompatActivity {
+
+    Button bt1, bt2, bt3, bt4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        bt1 = (Button) findViewById(R.id.button);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainPage.this, "Test 1 Complete", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainPage.this, Result1.class);
+                startActivity(intent);
+            }
+        });
+
+        bt2 = (Button) findViewById(R.id.button2);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainPage.this, "Test 2 Complete", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bt3 = (Button) findViewById(R.id.button4);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainPage.this, "Test 3 Complete", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bt4 = (Button) findViewById(R.id.button5);
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainPage.this, "Test 4 Complete", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
